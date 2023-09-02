3b138cdb890dd4215644f90a6cdc175d8ac0e1ce
Diego Perez <pharesdiego@gmail.com>
2023-09-02 14:57:57 -0500

    feat: fetch todos from API

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,25 +1,13 @@
-import { faker } from '@faker-js/faker'
 import type { PageServerLoad } from './$types';
-
-type Todo = {
-  id: string;
-  title: string;
-  content: string;
-};
-
-const createTodo = (): Todo => {
-  return {
-    id: faker.string.uuid(),
-    title: faker.word.words({ count: { min: 2, max: 4 } }),
-    content: faker.lorem.sentences()
-  };
-};
+import { fetchTodos } from '../services/todos';
 
 
-export const load: PageServerLoad = ({ params }) => {
-  const todos = Array.from({ length: 10 }, createTodo)
+export const load: PageServerLoad = async ({ params }) => {
+  const todos = await fetchTodos();
+
+  console.log(todos);
 
   return {
-    todos: todos,
+    todos
   };
 };
