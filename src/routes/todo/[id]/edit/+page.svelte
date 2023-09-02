22d8e38bd0f54f58c6c5b250b171e35e1545a6df
Diego Perez <pharesdiego@gmail.com>
2023-09-02 13:17:12 -0500

    feat: add todo edit page

---
+++ src/routes/todo/[id]/edit/+page.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+	import Header from "$lib/Header.svelte";
+	import TodoForm from "$lib/TodoForm.svelte";
+
+</script>
+
+<Header bangers>
+  Edit your todo
+</Header>
+
+<TodoForm
+  ctaText='Edit todo'
+  todo={{
+    title: 'edited',
+    content: 'dsadasdadD'
+  }}
+/>
