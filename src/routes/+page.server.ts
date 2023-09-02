9e6d613c5ebc35c163b5ff7d68a6d8d39946fb7d
Diego Perez <pharesdiego@gmail.com>
2023-09-02 15:30:32 -0500

    feat: define default action for create todo form

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,13 +1,22 @@
 import type { PageServerLoad } from './$types';
-import { fetchTodos } from '../services/todos';
+import { createTodo, fetchTodos } from '../services/todos';
+import { fail, type Actions } from '@sveltejs/kit';
 
 
 export const load: PageServerLoad = async ({ params }) => {
   const todos = await fetchTodos();
 
-  console.log(todos);
-
   return {
     todos
   };
 };
+
+export const actions = {
+  default: async ({ request }) => {
+    try {
+      await createTodo(await request.formData())
+    } catch (e) {
+      return fail(400, { message: "◉_◉" })
+    }
+  }
+} satisfies Actions;
