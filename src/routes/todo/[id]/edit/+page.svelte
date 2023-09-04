bfa424504ea29e18cfc11c582894c0bc17a8d4b1
Diego Perez <pharesdiego@gmail.com>
2023-09-04 15:49:31 -0500

    feat: show form status feedback when submitting edit form

--- src/routes/todo/[id]/edit/+page.svelte
+++ src/routes/todo/[id]/edit/+page.svelte
@@ -1,17 +1,32 @@
 <script lang="ts">
 	import Header from '$lib/Header.svelte';
 	import TodoForm from '$lib/TodoForm.svelte';
-	import type { PageServerData } from './$types';
-
+	import type { PageServerData, ActionData } from './$types';
+	
 	export let data: PageServerData;
+	export let form: ActionData;
+
+	let isUpdating = false
 </script>
 
 <Header bangers>Edit your todo</Header>
 
-<TodoForm
-	ctaText="Edit todo"
-	todo={{
-		title: data.todo.title,
-		content: data.todo.content
-	}}
-/>
+<section>
+	<TodoForm
+		onFormStatusChange={(status) => isUpdating = status === 'submitting'}
+		shouldReset={false}
+		ctaText="Edit todo"
+		todo={{
+			title: data.todo.title,
+			content: data.todo.content
+		}}
+	/>
+</section>
+
+{#if isUpdating}
+	<p>Updating...</p>
+{/if}
+
+{#if form?.success && isUpdating === false}
+	<p>Successfully updated!</p>
+{/if}
