8558369b2ad9e6c9c7b729caac68fee32ce5ed33
Diego Perez <pharesdiego@gmail.com>
2023-09-07 15:07:37 -0500

    feat: add tests using playwright
    these tests are not supposed to actually follow good practices. These
    tests are not ran in a controlled environment where variables/changes
    could be tested deterministically, these are done only for exploring the
    playwright lib

---
+++ tests/edit.test.ts
@@ -0,0 +1,38 @@
+import { expect, test, type Page, type Request } from '@playwright/test';
+
+test.beforeEach(async ({ page }) => {
+  await page.goto('/')
+
+  await page.getByTestId('todo').first().getByRole('link', { name: 'Edit' }).click()
+
+  await page.waitForURL('**/edit')
+});
+
+
+test("form fields can't be empty", async ({ page }) => {
+  const requests: Request[] = []
+
+  page.on('request', (request) => requests.push(request))
+
+  await page.getByLabel('Title').fill('')
+  await page.getByLabel('Content').fill('')
+
+  await page.getByRole('button', { name: 'Edit todo' }).click()
+
+  expect(requests).toHaveLength(0)
+})
+
+test("user is able to edit and submit form", async ({ page }) => {
+  const editRequest = page.waitForResponse((response) => response.url().includes('/edit'))
+
+  await page.getByLabel('Title').fill('__title')
+  await page.getByLabel('Content').fill('__content')
+
+  await page.getByRole('button', { name: 'Edit todo' }).click()
+
+  await expect(page.getByTestId('edit-loading-msg')).toBeVisible()
+
+  await editRequest;
+
+  await expect(page.getByTestId('edit-done-msg')).toBeVisible()
+})
---
+++ tests/index.test.ts
@@ -0,0 +1,47 @@
+import { expect, test, type Page } from '@playwright/test';
+
+const addTodo = async (title: string, content: string, page: Page) => {
+	await page.getByLabel('Title').fill(title)
+	await page.getByLabel('Content').fill(content)
+
+	await page.getByRole('button', { name: 'Add todo' }).click()
+
+	await page.getByText(title).waitFor({ 'state': 'attached' })
+}
+
+test.beforeEach(async ({ page }) => {
+	await page.goto('/')
+})
+
+test('index page has expected h1', async ({ page }) => {
+	await expect(page.getByRole('heading', { name: "Mbappé's favorite todos app" })).toBeVisible();
+});
+
+test('form creates new todo and shows it in the list', async ({ page }) => {
+	// Create todo
+	await addTodo('My title', 'My content', page)
+
+	// Make sure it was added
+	const todo = await page.getByTestId('todo').first()
+
+	await expect(todo.getByText('My title')).toBeVisible();
+	await expect(todo.getByText('My content')).toBeVisible();
+});
+
+test('"remove" button removes todo from list by calling endpoint', async ({ page }) => {
+	const unique_title = Math.random().toString()
+
+	await addTodo(unique_title, 'content', page)
+
+	const deleteRequest = page.waitForRequest(request => {
+		const result = request.url().includes('api/todos/') && request.method() === 'DELETE'
+
+		return result
+	})
+
+	await page.getByRole('button', { name: 'Remove' }).first().click()
+
+	await deleteRequest
+
+	await expect(page.getByText(unique_title)).not.toBeAttached()
+});
